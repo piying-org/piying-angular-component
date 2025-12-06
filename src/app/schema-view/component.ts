9bcfdf29d0aa9bcb8b03ce20ead5f97b35ef5c40
chen yang <wszgrcy@gmail.com>
2025-12-06 10:18:39 +0800

    selectorless

--- src/app/schema-view/component.ts
+++ src/app/schema-view/component.ts
@@ -1,24 +1,31 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, resource } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PiyingView } from '@piying/view-angular';
 import { AsyncPipe } from '@angular/common';
 import { FieldGlobalConfig } from '../piying/define';
+import { SelectorlessOutlet } from '@cyia/ngx-common/directive';
 const defaultValue = Promise.resolve(undefined);
 @Component({
   selector: 'app-schema-view',
   templateUrl: './component.html',
   standalone: true,
-  imports: [PiyingView, AsyncPipe],
+  imports: [PiyingView, AsyncPipe, SelectorlessOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SchemaViewRC {
   route = inject(ActivatedRoute);
-
+  readonly PiyingView = PiyingView;
   context = this.route.snapshot.data['context']?.();
   schema = this.route.snapshot.data['schema']();
-  model = this.route.snapshot.data['model']?.() || defaultValue;
+  model = resource({ loader: () => this.route.snapshot.data['model']?.() || defaultValue });
   options = {
     context: this.context,
     fieldGlobalConfig: FieldGlobalConfig,
+  };
+  inputs = {
+    schema: () => this.schema,
+    options: () => this.options,
+    model: this.model.value,
+    selectorless: () => true,
   };
 }
