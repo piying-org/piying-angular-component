722f21c70223e21c652c334cf1beec079fa5b478
chen yang <wszgrcy@gmail.com>
2025-12-14 09:40:17 +0800

    table style

--- src/app/piying/page/demo/query-table.ts
+++ src/app/piying/page/demo/query-table.ts
@@ -1,5 +1,6 @@
 import * as v from 'valibot';
 import {
+  bottomClass,
   formConfig,
   hideWhen,
   mergeHooks,
@@ -88,200 +89,215 @@ const QueryCondition = v.pipe(
   setWrappers(['div']),
   topClass('flex justify-between'),
 );
-const TableDefine = v.object({
-  table: v.pipe(
-    NFCSchema,
-    setComponent('table'),
-    setWrappers(['table-status', 'sort-table', 'table-resource', 'checkbox-table']),
+const TableDefine = v.pipe(
+  v.object({
+    table: v.pipe(
+      NFCSchema,
+      setComponent('table'),
+      setWrappers(['table-status', 'sort-table', 'table-resource', 'checkbox-table']),
 
-    patchAsyncInputs({
-      define: (field) => {
-        let pageFiled = field.get(['..', 'page']);
-        return {
-          row: {
-            head: [{ columns: ['checkbox', 'index', 'title', 'level', 'badge', 'actions'] }],
-            body: [
-              {
-                define: v.pipe(v.tuple([]), setComponent('tr')),
-                columns: ['checkbox', 'index', 'title', 'level', 'badge', 'actions'],
+      patchAsyncInputs({
+        define: (field) => {
+          let pageFiled = field.get(['..', 'bottom', 'page']);
+          return {
+            row: {
+              head: [
+                {
+                  columns: ['checkbox', 'index', 'title', 'level', 'badge', 'actions'],
+                  define: v.pipe(v.tuple([]), setComponent('tr'), topClass('bg-base-200')),
+                },
+              ],
+              body: [
+                {
+                  define: v.pipe(v.tuple([]), setComponent('tr')),
+                  columns: ['checkbox', 'index', 'title', 'level', 'badge', 'actions'],
+                },
+              ],
+            },
+            columns: {
+              checkbox: {
+                head: ' ',
+                body: v.pipe(
+                  v.boolean(),
+                  setComponent('checkbox'),
+                  setWrappers(['td', 'table-checkbox-body']),
+                ),
               },
-            ],
-            foot: [{ columns: ['add'] }],
-          },
-          columns: {
-            checkbox: {
-              head: ' ',
-              body: v.pipe(
-                v.boolean(),
-                setComponent('checkbox'),
-                setWrappers(['td', 'table-checkbox-body']),
-              ),
-            },
-            index: {
-              head: '索引',
-              body: (node: any, index: number) => {
-                let { pageQueryParams } = pageFiled?.props()!;
-                return `${index + 1 + pageQueryParams.index * pageQueryParams.size}`;
+              index: {
+                head: '索引',
+                body: (node: any, index: number) => {
+                  let { pageQueryParams } = pageFiled?.props()!;
+                  return `${index + 1 + pageQueryParams.index * pageQueryParams.size}`;
+                },
               },
-            },
-            title: {
-              head: '标题',
-              body: (data: any) => {
-                return data.title;
+              title: {
+                head: '标题',
+                body: (data: any) => {
+                  return data.title;
+                },
               },
-            },
-            level: {
-              head: '等级',
-              body: (data: any) => {
-                return LevelObj[data.level];
+              level: {
+                head: '等级',
+                body: (data: any) => {
+                  return LevelObj[data.level];
+                },
               },
-            },
-            badge: {
-              head: 'badge',
-              body: v.pipe(
-                NFCSchema,
-                setComponent('badge'),
-                setWrappers(['td']),
-                patchAsyncInputs({
-                  content: ({ context }) => {
-                    return computed(() => context.item$().badge);
-                  },
-                }),
-              ),
-            },
-            actions: {
-              head: '操作',
-              body: v.pipe(
-                v.object({
-                  edit: v.pipe(
-                    NFCSchema,
-                    setComponent('button'),
-                    patchInputs({
-                      content: { icon: { fontIcon: 'edit' } },
-                      shape: 'circle',
-                      size: 'sm',
-                    }),
-                    patchAsyncInputs({
-                      clicked: (field) => {
-                        return () => {
-                          console.log(field.context['item$']());
-                        };
-                      },
-                    }),
-                  ),
-                  delete: v.pipe(
-                    NFCSchema,
-                    setComponent('button'),
-                    patchInputs({
-                      content: { icon: { fontIcon: 'delete' } },
-                      shape: 'circle',
-                      size: 'sm',
-                    }),
-                    topClass('text-error'),
-                    patchAsyncInputs({
-                      clicked: (field) => {
-                        return () => {
-                          console.log(field.context['item$']());
-                        };
-                      },
-                    }),
-                  ),
-                }),
-                topClass('flex gap-2'),
-                setWrappers(['td']),
-              ),
-            },
-            add: {
-              foot: v.pipe(
-                NFCSchema,
-                setComponent('button'),
-                patchInputs({ content: 'add' }),
-                patchAsyncInputs({
-                  clicked: (field) => {
-                    return () => {
-                      let dialog: DialogService = field.context['dialog'];
-                      dialog.openDialog({
-                        title: '添加',
-                        schema: FormBase,
-                        applyValue: (value) => {
-                          // 更新或添加
+              badge: {
+                head: 'badge',
+                body: v.pipe(
+                  NFCSchema,
+                  setComponent('badge'),
+                  setWrappers(['td']),
+                  patchAsyncInputs({
+                    content: ({ context }) => {
+                      return computed(() => context.item$().badge);
+                    },
+                  }),
+                ),
+              },
+              actions: {
+                head: '操作',
+                body: v.pipe(
+                  v.object({
+                    edit: v.pipe(
+                      NFCSchema,
+                      setComponent('button'),
+                      patchInputs({
+                        content: { icon: { fontIcon: 'edit' } },
+                        shape: 'circle',
+                        size: 'sm',
+                      }),
+                      patchAsyncInputs({
+                        clicked: (field) => {
+                          return () => {
+                            console.log(field.context['item$']());
+                          };
                         },
-                      });
-                    };
-                  },
-                }),
-              ),
+                      }),
+                    ),
+                    delete: v.pipe(
+                      NFCSchema,
+                      setComponent('button'),
+                      patchInputs({
+                        content: { icon: { fontIcon: 'delete' } },
+                        shape: 'circle',
+                        size: 'sm',
+                      }),
+                      topClass('text-error'),
+                      patchAsyncInputs({
+                        clicked: (field) => {
+                          return () => {
+                            console.log(field.context['item$']());
+                          };
+                        },
+                      }),
+                    ),
+                  }),
+                  topClass('flex gap-2'),
+                  setWrappers(['td']),
+                ),
+              },
             },
-          },
-        };
-      },
-    }),
-    patchProps({ sortList: ['title', 'level'] }),
-    patchAsyncProps({
-      data: (field) => {
-        let init = range(100).map((item) => {
-          return {
-            title: faker.animal.type(),
-            level: faker.number.int(2),
-            badge: faker.animal.dog(),
           };
-        });
-        let value = signal(init);
-        field
-          .get(['..', '..', 'query', 'params'])!
-          .form.control!.valueChanges.subscribe((searchObj) => {
-            if (!searchObj) {
-              value.set(init);
-            } else {
-              let list = init;
-              if (searchObj.content) {
-                let content = (searchObj.content as string).toLowerCase();
-                list = init.filter((item) => item.title.toLowerCase().includes(content));
-              } else {
-                list = init;
-              }
-              if (searchObj.level) {
-                list = list.filter((item) => item.level === searchObj.level);
-              }
-              value.set(list);
-            }
+        },
+      }),
+      patchProps({ sortList: ['title', 'level'] }),
+      patchAsyncProps({
+        data: (field) => {
+          let init = range(100).map((item) => {
+            return {
+              title: faker.animal.type(),
+              level: faker.number.int(2),
+              badge: faker.animal.dog(),
+            };
           });
-        return value;
-      },
-    }),
-    patchAsyncProps({
-      queryParams: (field) => {
-        let { props } = field;
-        let pageProps = field.get(['..', 'page'])?.props;
-        return computed(() => {
-          return {
-            // page field
-            page: pageProps?.()['pageQueryParams'],
-            // sort-table
-            direction: props()['sortQueryParams'],
-          };
-        });
-      },
-    }),
-  ),
-  page: v.pipe(
-    NFCSchema,
-    setComponent('pagination'),
-    topClass('mt-2'),
-    patchInputs({
-      value: {
-        size: 10,
-        index: 0,
-      },
-    }),
-    patchAsyncInputs({
-      count: (field) => {
-        let tableField = field.get(['..', 'table'])!;
-        return computed(() => {
-          return tableField.props()['count$$']();
-        });
-      },
-    }),
-  ),
-});
-export const QueryTableDefine = v.pipe(v.object({ query: QueryCondition, table: TableDefine }));
+          let value = signal(init);
+          field
+            .get(['..', '..', 'query', 'params'])!
+            .form.control!.valueChanges.subscribe((searchObj) => {
+              if (!searchObj) {
+                value.set(init);
+              } else {
+                let list = init;
+                if (searchObj.content) {
+                  let content = (searchObj.content as string).toLowerCase();
+                  list = init.filter((item) => item.title.toLowerCase().includes(content));
+                } else {
+                  list = init;
+                }
+                if (searchObj.level) {
+                  list = list.filter((item) => item.level === searchObj.level);
+                }
+                value.set(list);
+              }
+            });
+          return value;
+        },
+      }),
+      patchAsyncProps({
+        queryParams: (field) => {
+          let { props } = field;
+          let pageProps = field.get(['..', 'bottom', 'page'])?.props;
+          return computed(() => {
+            return {
+              // page field
+              page: pageProps?.()['pageQueryParams'],
+              // sort-table
+              direction: props()['sortQueryParams'],
+            };
+          });
+        },
+      }),
+      bottomClass('mt-4 border rounded-box border-base-content/5'),
+    ),
+    bottom: v.pipe(
+      v.object({
+        add: v.pipe(
+          NFCSchema,
+          setComponent('button'),
+          patchInputs({ content: { icon: { fontIcon: 'add' }, title: 'add' } }),
+          patchAsyncInputs({
+            clicked: (field) => {
+              return () => {
+                let dialog: DialogService = field.context['dialog'];
+                dialog.openDialog({
+                  title: '添加',
+                  schema: FormBase,
+                  applyValue: (value) => {
+                    // 更新或添加
+                  },
+                });
+              };
+            },
+          }),
+        ),
+        page: v.pipe(
+          NFCSchema,
+          setComponent('pagination'),
+          topClass('mt-4 flex justify-end'),
+          patchInputs({
+            value: {
+              size: 10,
+              index: 0,
+            },
+          }),
+          patchAsyncInputs({
+            count: (field) => {
+              let tableField = field.get(['..', '..', 'table'])!;
+              return computed(() => {
+                return tableField.props()['count$$']();
+              });
+            },
+          }),
+        ),
+      }),
+      setWrappers(['div']),
+      topClass('flex justify-between items-center'),
+    ),
+  }),
+);
+export const QueryTableDefine = v.pipe(
+  v.object({ query: QueryCondition, table: TableDefine }),
+  setWrappers(['div']),
+  topClass('m-4'),
+);
