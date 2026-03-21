b1f325c464562a472362fb42b6f3e67fcbf4b2b7
chen <wszgrcy@gmail.com>
2026-03-21 17:38:29 +0800

    header

--- src/app/piying/component/code-header/component.ts
+++ src/app/piying/component/code-header/component.ts
@@ -6,6 +6,7 @@ import {
   signal,
   viewChild,
 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SelectorlessOutlet } from '@cyia/ngx-common/directive';
 import { StrOrTemplateComponent } from '@piying-lib/angular-core';
 import { PI_INPUT_OPTIONS_TOKEN, PiyingView, PiyingViewGroupBase } from '@piying/view-angular';
@@ -20,4 +21,6 @@ export class CodeHeaderNFCC {
   static __version = 2;
   templateRef = viewChild.required('templateRef');
   title = input<string>();
+
+  location = location;
 }
