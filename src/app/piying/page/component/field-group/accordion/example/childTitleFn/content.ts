3263103952cd19c40cc627d86dbb7129c7d0280c
chen <wszgrcy@gmail.com>
2026-03-22 09:51:28 +0800

    doc

--- src/app/piying/page/component/field-group/accordion/example/childTitleFn/content.ts
+++ src/app/piying/page/component/field-group/accordion/example/childTitleFn/content.ts
@@ -4,11 +4,15 @@ import { safeDefine } from '@@piying-define';
 export default v.pipe(
   v.tuple([
     v.pipe(
-      NFCSchema,
+      v.object({
+        l1: v.pipe(v.string(), v.title('l1-title')),
+        l2: v.pipe(v.boolean(), v.title('l2-title')),
+      }),
       safeDefine.setComponent('accordion', (actions) => {
         return [
           actions.inputs.patch({
-            childTitleFn: (item: any) => `📌 ${item.props()?.['title'] ?? item.keyPath?.slice(-1)[0]}`,
+            childTitleFn: (item: any) =>
+              `📌 ${item.props()?.['title'] ?? item.keyPath?.slice(-1)[0]}`,
           }),
         ];
       }),
--- src/app/piying/page/component/field-group/list/example/titleClass/content.ts
+++ src/app/piying/page/component/field-group/list/example/titleClass/content.ts
@@ -4,7 +4,10 @@ import { safeDefine } from '@@piying-define';
 export default v.pipe(
   v.tuple([
     v.pipe(
-      NFCSchema,
+      v.object({
+        l1: v.pipe(v.string(), v.title('l1-title')),
+        l2: v.pipe(v.boolean(), v.title('l2-title')),
+      }),
       safeDefine.setComponent('list', (actions) => {
         return [
           actions.inputs.patch({
