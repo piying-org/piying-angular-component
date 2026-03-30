2093e9774e13527a0b4c46b703d4ffb740e176f3
chen <wszgrcy@gmail.com>
2026-03-30 22:02:33 +0800

    loading

---
+++ projects/ionic/wrapper/loading/component.ts
@@ -0,0 +1,20 @@
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, viewChild, input, output, computed, inject } from '@angular/core';
+import { IonLoading } from '@ionic/angular/standalone';
+import { JSX } from '@ionic/core';
+import { AttributesDirective, PI_VIEW_FIELD_TOKEN } from '@piying/view-angular';
+type Prop = JSX.IonLoading;
+
+@Component({
+  selector: 'app-ion-loading',
+  templateUrl: './component.html',
+  imports: [AttributesDirective, NgTemplateOutlet, IonLoading],
+})
+export class IonLoadingWC {
+  static __version = 2;
+  templateRef = viewChild.required('templateRef');
+  field$$ = inject(PI_VIEW_FIELD_TOKEN);
+  props = computed(() => {
+    return this.field$$().props()['Loading'] as Prop | undefined;
+  });
+}
