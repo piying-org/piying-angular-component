bd7fa7570e9f7749ce5c475f6a8a4d625217f874
chen <wszgrcy@gmail.com>
2026-03-30 19:04:38 +0800

    IonInfiniteScroll

---
+++ projects/ionic/wrapper/infinite-scroll/component.ts
@@ -0,0 +1,37 @@
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, viewChild, input, output, computed, inject } from '@angular/core';
+import {
+  AttributesDirective,
+  InsertFieldDirective,
+  PI_VIEW_FIELD_TOKEN,
+} from '@piying/view-angular';
+import { JSX } from '@ionic/core';
+import { IonInfiniteScroll, IonInfiniteScrollContent } from '@ionic/angular/standalone';
+
+type Prop = JSX.IonInfiniteScroll;
+type Prop2Content = JSX.IonInfiniteScrollContent;
+
+@Component({
+  selector: 'app-ion-infinite-scroll',
+  templateUrl: './component.html',
+  imports: [
+    AttributesDirective,
+    NgTemplateOutlet,
+    InsertFieldDirective,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+  ],
+})
+export class IonInfiniteScrollWC {
+  static __version = 2;
+  templateRef = viewChild.required('templateRef');
+  field$$ = inject(PI_VIEW_FIELD_TOKEN);
+  props = computed(() => {
+    return this.field$$().props()['InfiniteScroll'] as Prop | undefined;
+  });
+  props2 = computed(() => {
+    return this.field$$().props()['IonInfiniteScrollContent'] as Prop2Content | undefined;
+  });
+
+  ionInfinite = output<Parameters<NonNullable<Prop['onIonInfinite']>>[0]>();
+}
