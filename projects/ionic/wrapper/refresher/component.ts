db5b57b23f7d83c97af30f0fa623aca5fc95e1b8
chen <wszgrcy@gmail.com>
2026-03-30 19:54:50 +0800

    refresher

---
+++ projects/ionic/wrapper/refresher/component.ts
@@ -0,0 +1,25 @@
+import { NgTemplateOutlet } from '@angular/common';
+import { Component, viewChild, input, computed, inject } from '@angular/core';
+import { AttributesDirective, PI_VIEW_FIELD_TOKEN } from '@piying/view-angular';
+import { JSX } from '@ionic/core';
+import { IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
+type Prop = JSX.IonRefresher;
+type Prop2Content = JSX.IonRefresherContent;
+
+@Component({
+  selector: 'app-ion-refresher-content',
+  templateUrl: './component.html',
+  imports: [AttributesDirective, IonRefresher, IonRefresherContent],
+})
+export class IonRefresherWC {
+  static __version = 2;
+  templateRef = viewChild.required('templateRef');
+  field$$ = inject(PI_VIEW_FIELD_TOKEN);
+
+  props = computed(() => {
+    return this.field$$().props()['InfiniteScroll'] as Prop | undefined;
+  });
+  props2 = computed(() => {
+    return this.field$$().props()['IonInfiniteScrollContent'] as Prop2Content | undefined;
+  });
+}
