2b690b9dd6c1c76a9c6e67084daa4a5a1e3c7d37
chen <wszgrcy@gmail.com>
2026-03-30 21:06:25 +0800

    title

--- projects/ionic/non-field-control/title/component.ts
+++ projects/ionic/non-field-control/title/component.ts
@@ -2,17 +2,17 @@ import { Component, viewChild, input } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
 import { AttributesDirective } from '@piying/view-angular';
 import { JSX } from '@ionic/core';
+import { IonTitle } from '@ionic/angular/standalone';
 type Prop = JSX.IonTitle;
 
 @Component({
   selector: 'app-ion-title',
   templateUrl: './component.html',
-  imports: [AttributesDirective, NgTemplateOutlet],
+  imports: [AttributesDirective, IonTitle],
 })
 export class IonTitleNFCC {
   static __version = 2;
   templateRef = viewChild.required('templateRef');
   color = input<Prop['color']>();
   size = input<Prop['size']>();
-  // ionStyle = output<Parameters<NonNullable<Prop['onIonStyle']>>[0]>();
 }
