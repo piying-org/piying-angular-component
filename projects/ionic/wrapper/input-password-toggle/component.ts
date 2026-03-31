9d8d68abc61e43588a6e101296f10186d80469b6
chen <wszgrcy@gmail.com>
2026-03-31 09:04:49 +0800

    password

---
+++ projects/ionic/wrapper/input-password-toggle/component.ts
@@ -0,0 +1,34 @@
+import { Component, viewChild, input, inject, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonInputPasswordToggle } from '@ionic/angular/standalone';
+import { JSX } from '@ionic/core';
+import { PI_VIEW_FIELD_TOKEN } from '@piying/view-angular';
+type Prop = JSX.IonInputPasswordToggle;
+
+@Component({
+  selector: 'app-ion-input-password-toggle',
+  templateUrl: './component.html',
+  imports: [IonInputPasswordToggle, FormsModule],
+})
+export class IonInputPasswordToggleWC {
+  static __version = 2;
+  templateRef = viewChild.required('templateRef');
+  color = input<Prop['color']>();
+  hideIcon = input<Prop['hideIcon']>();
+  showIcon = input<Prop['showIcon']>();
+  mode = input<Prop['mode']>();
+  slot = viewChild.required('slot');
+
+  field$$ = inject(PI_VIEW_FIELD_TOKEN);
+  props = computed(() => {
+    return this.field$$().props()['InputPasswordToggle'] as Prop | undefined;
+  });
+  ngOnInit(): void {
+    this.field$$().inputs.update((value) => {
+      return {
+        ...value,
+        slot: this.slot(),
+      };
+    });
+  }
+}
