b9734e8a659a9f4f7bc55675416c68ec7195fb63
chen <wszgrcy@gmail.com>
2026-04-04 09:56:26 +0800

    navbar slot

--- projects/daisyui/field-group/navbar/component.ts
+++ projects/daisyui/field-group/navbar/component.ts
@@ -34,12 +34,17 @@ export class NavbarFGC extends PiyingViewGroupBase {
     });
   });
   rest$$ = computed(() => {
-    return this.children$$().filter((field) => {
-      return (
-        field.keyPath?.slice(-1)[0] !== 'start' &&
-        field.keyPath?.slice(-1)[0] !== 'center' &&
-        field.keyPath?.slice(-1)[0] !== 'end'
-      );
+    let defaultSlot = this.children$$().find((field) => {
+      return field.keyPath?.slice(-1)[0] === 'default';
     });
+    return defaultSlot
+      ? [defaultSlot]
+      : this.children$$().filter((field) => {
+          return (
+            field.keyPath?.slice(-1)[0] !== 'start' &&
+            field.keyPath?.slice(-1)[0] !== 'center' &&
+            field.keyPath?.slice(-1)[0] !== 'end'
+          );
+        });
   });
 }
